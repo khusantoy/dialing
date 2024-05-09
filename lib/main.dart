7d4fa30a7b2ea5c5a791944965c4a4d08b41ce51
Khusanboy Tursunov <tursunovkhusanboy@gmail.com>
2024-05-09 09:35:14 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,477 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String number = '+998121231212';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  children: [
+                    Text(
+                      number,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        color: Colors.black87,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const Text(
+                      "Add Number",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            number = '1';
+                          });
+                        },
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "1",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "2",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "ABC",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "3",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "DEF",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            number = '1';
+                          });
+                        },
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "4",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "GHI",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "5",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "JKL",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "6",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "MNO",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            number = '1';
+                          });
+                        },
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "7",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "PQRS",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "8",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "TUV",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "9",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "WXYZ",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "*",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            height: 0.9,
+                            fontSize: 40,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "0",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                height: 0.9,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              "+",
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.grey.shade300,
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          "#",
+                          style: TextStyle(
+                            color: Colors.black87,
+                            height: 0.9,
+                            fontSize: 40,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 90,
+                      height: 90,
+                      child: IconButton(
+                        style: IconButton.styleFrom(
+                          backgroundColor: Colors.green.shade400,
+                        ),
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.phone,
+                          size: 50,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
